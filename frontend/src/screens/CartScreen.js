6d74c105c01ccea68e44f9bf4ba43e1c3acb005f
giannisgkountras <gkountrasioannis@gmail.com>
2022-08-26 02:33:41 +0300

    add to cart Redux and badge

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useParams, useSearchParams } from "react-router-dom";
+import { addToCart } from "../actions/cartActions";
 
 export default function CartScreen() {
   const productId = useParams();
   const [searchParams] = useSearchParams();
   const qty = [searchParams] ? Number(searchParams.get("qty")) : 1;
-
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId.id, qty));
+    }
+  }, [dispatch, productId, qty]);
   return (
     <div>
       <h1>CartScreen</h1>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,11 +2,17 @@ import { combineReducers, compose, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
 import { legacy_createStore as createStore } from "redux";
 import { productDetailsReducer, productListReducer } from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
 
-const initialState = {};
+const initialState = {
+  cart: {
+    cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
+  },
+};
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
